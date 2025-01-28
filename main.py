e6d99a522fc7d4bf7943a1aee254cb025e542601
Mohamed Nasr <mohammedalsharkawy22@gmail.com>
2025-01-28 09:59:13 -0800

    complete training

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ from src.datascience import logger
 from src.datascience.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
 from src.datascience.pipeline.data_validation_pipeline import DataValidationTrainingPipeline
 from src.datascience.pipeline.data_transformation_pipeline import DataTransformationTrainingPipeline
+from src.datascience.pipeline.model_trainer_pipeline import ModelTrainerTrainingPipeline
+
+
+
 
 logger.info("welcome to our custom datascience projec")
 
@@ -36,3 +40,12 @@ except Exception as e:
         raise e
 
 
+STAGE_NAME = "Model Trainer stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = ModelTrainerTrainingPipeline()
+   data_ingestion.initiate_model_training()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
